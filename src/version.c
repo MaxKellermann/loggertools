3ef48b3ac49901d08467f3b74bd7375c9ccd6bbf
Max Kellermann <max@duempel.org>
2007-02-26 07:26:06 +0000

    added bin/version
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/loggertools/trunk@282 fae35197-53e2-0310-b5ce-cc51903860e9

---
+++ src/version.c
@@ -0,0 +1,31 @@
+/*
+ * loggertools
+ * Copyright (C) 2004-2007 Max Kellermann <max@duempel.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * $Id: version.h 239 2007-02-23 06:45:46Z max $
+ */
+
+#include "version.h"
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    puts(VERSION);
+}
