a779ce8501146e845c3508ccba13f57eefa729a7
Max Kellermann <max@duempel.org>
2007-08-02 13:09:09 +0000

    removed svn:keywords
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/loggertools/trunk@455 fae35197-53e2-0310-b5ce-cc51903860e9

--- src/filser-tool.c
+++ src/filser-tool.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
  * 02111-1307, USA.
  *
- * $Id$
+ * $Id: filser-tool.c 318 2007-03-05 12:53:41Z max $
  */
 
 #include <assert.h>
--- src/lxn2igc.c
+++ src/lxn2igc.c
@@ -1,6 +1,6 @@
 /*
  * loggertools
- * Copyright (C) 2004-2007 Max Kellermann (max@duempel.org)
+ * Copyright (C) 2004-2007 Max Kellermann <max@duempel.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -15,8 +15,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
  * 02111-1307, USA.
- *
- * $Id$
  */
 
 #include <assert.h>
