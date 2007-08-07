9cfb30164fbb6446d6e4e7fd9a93d58af672be6d
Max Kellermann <max@duempel.org>
2007-08-07 11:41:38 +0000

    relicense fifo-buffer.c under LGPL 2.1
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/loggertools/trunk@466 fae35197-53e2-0310-b5ce-cc51903860e9

--- src/fifo-buffer.h
+++ src/fifo-buffer.h
@@ -1,20 +1,20 @@
 /*
- * loggertools
  * Copyright (C) 2004-2007 Max Kellermann <max@duempel.org>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; version 2 of the License
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; version 2.1 of the
+ * License.
  *
- * This program is distributed in the hope that it will be useful, but
+ * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
  *
  * $Id$
  */
