62b363d1d70c9b713a96371d2ca4f16c5f90f641
Max Kellermann <max@duempel.org>
2008-05-16 19:56:35 +0000

    fix FL rounding
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/loggertools/trunk@560 fae35197-53e2-0310-b5ce-cc51903860e9

--- src/airspace-openair-writer.cc
+++ src/airspace-openair-writer.cc
@@ -125,7 +125,7 @@ static std::ostream &operator <<(std::ostream &os, const Altitude &alt) {
     }
 
     if (ref == Altitude::REF_1013)
-        return os << ref << ((value + 499) / 100);
+        return os << ref << ((value + 49) / 100);
     else
         return os << std::setfill('0') << std::setw(4) << value << ref;
 }
